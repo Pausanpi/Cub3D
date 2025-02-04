749f5f32c6245e189e1c7ce5955f75da8d4241db
Laureano Cuevas Melendez <lcuevas-@c2r4s6.42malaga.com>
2025-02-04 12:10:40 +0100

    norma

--- src/parse.c
+++ src/parse.c
@@ -12,25 +12,25 @@
 
 #include "../libs/cub3d.h"
 
-int parser(t_data *data, char *file)
+int	parser(t_data *data, char *file)
 {
-    char *texture;
+	char	*texture;
 
-    texture = NULL;
-    data->fd = open(file, O_RDONLY);
-    if (data->fd == -1)
-        return (printf("Error: Invalid file\n"), 1);
-    data->line = get_next_line(data->fd);
-    if (!data->line)
-        return (printf("Error: Empty file\n"), 1);
-    if (check_textures(data, &texture) == 1)
+	texture = NULL;
+	data->fd = open(file, O_RDONLY);
+	if (data->fd == -1)
+		return (printf("Error: Invalid file\n"), 1);
+	data->line = get_next_line(data->fd);
+	if (!data->line)
+		return (printf("Error: Empty file\n"), 1);
+	if (check_textures(data, &texture) == 1)
 	{
-        return (1);
+		return (1);
 	}
 	load_textures(data, texture); // habria que hacer un check de esto
 	if (!data->no || !data->so || !data->ea || !data->we)
-		return(free(data->line), printf("Error: Missing textures\n"), 1);
+		return (free(data->line), printf("Error: Missing textures\n"), 1);
 	if (!data->ceiling || !data->floor)
-		return(free(data->line), printf("Error: Missing RGB colors\n"), 1);
-    return (0);
+		return (free(data->line), printf("Error: Missing RGB colors\n"), 1);
+	return (0);
 }
