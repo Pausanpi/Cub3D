bb307899551b62afc485113de56bb24656c75943
Paula <pausanpi1606@gmail.com>
2025-02-05 13:30:29 +0100

    refactor: improve ft_paint_walls function and introduce t_paint struct

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: pausanch <pausanch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/03 11:08:24 by pausanch          #+#    #+#             */
-/*   Updated: 2025/02/05 13:12:50 by pausanch         ###   ########.fr       */
+/*   Updated: 2025/02/05 13:29:57 by pausanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,11 @@ float ft_distance(t_data *data, mlx_texture_t **current)
 {
 	float wall_x;
 	if (data->ray->last_cross == 0)
-		wall_x = data->ray->wall_collision.y - floor(data->ray->wall_collision.y);
+		wall_x = data->ray->wall_collision.y
+			- floor(data->ray->wall_collision.y);
 	else
-		wall_x = data->ray->wall_collision.x - floor(data->ray->wall_collision.x);
-
+		wall_x = data->ray->wall_collision.x
+			- floor(data->ray->wall_collision.x);
 	if (data->ray->last_cross == 0)
 	{
 		if (data->ray->x_sign == 1)
@@ -37,46 +38,51 @@ float ft_distance(t_data *data, mlx_texture_t **current)
 	return (wall_x * (*current)->width);
 }
 
-void	ft_paint_walls(t_data *f, float wall, int col)
+void ft_peint(t_data *f, int col, t_paint *p)
 {
-	int		j;
-	int		top;
-	int		bot;
-	float	tex_x;
-	float	tex_y;
-	float	step;
-	mlx_texture_t *current_texture;
-	uint8_t	*pixel;
-
-	current_texture = malloc(sizeof(mlx_texture_t));
-	tex_x = ft_distance(f, &current_texture);
-	top = HEIGHT / 2 - wall / 2;
-	if (top < 0)
-		top = 0;
-	bot = top + wall;
-	if (bot > HEIGHT)
-		bot = HEIGHT;
-	step = (float)current_texture->height / wall;
-	tex_y = 0;
-	if (top == 0)
-		tex_y = -(HEIGHT / 2 - wall / 2) * step;
+	int j;
 
 	j = 0;
 	while (j < HEIGHT)
 	{
-		if (j < top)
-			mlx_put_pixel(f->img, col, j, (f->ceiling[0] << 24 | f->ceiling[1] << 16 | f->ceiling[2] << 8 | 255)); // Ceiling
-		else if (j > bot)
-			mlx_put_pixel(f->img, col, j, (f->floor[0] << 24 | f->floor[1] << 16 | f->floor[2] << 8 | 255)); // Floor
+		if (j < p->top)
+			mlx_put_pixel(f->img, col, j, (f->ceiling[0] << 24
+				| f->ceiling[1] << 16 | f->ceiling[2] << 8 | 255));
+		else if (j > p->bot)
+			mlx_put_pixel(f->img, col, j, (f->floor[0] << 24
+				| f->floor[1] << 16 | f->floor[2] << 8 | 255));
 		else
 		{
-			int tex_y_int = (int)tex_y % current_texture->height;
-			pixel = &current_texture->pixels[
-				(tex_y_int * current_texture->width + (int)tex_x) * current_texture->bytes_per_pixel];
-			uint32_t color = (pixel[0] << 24) | (pixel[1] << 16) | (pixel[2] << 8) | 255;
+			int tex_y_int = (int)p->tex_y % p->current_texture->height;
+			p->pixel = &p->current_texture->pixels[
+				(tex_y_int * p->current_texture->width + (int)p->tex_x)
+					* p->current_texture->bytes_per_pixel];
+			uint32_t color = (p->pixel[0] << 24)
+				| (p->pixel[1] << 16) | (p->pixel[2] << 8) | 255;
 			mlx_put_pixel(f->img, col, j, color);
-			tex_y += step;
+			p->tex_y += p->step;
 		}
 		j++;
 	}
+}
+
+void	ft_paint_walls(t_data *f, float wall, int col)
+{
+	t_paint	*p;
+
+	p = malloc(sizeof(t_paint));
+	p->current_texture = malloc(sizeof(mlx_texture_t));
+	p->tex_x = ft_distance(f, &p->current_texture);
+	p->top = HEIGHT / 2 - wall / 2;
+	if (p->top < 0)
+		p->top = 0;
+	p->bot = p->top + wall;
+	if (p->bot > HEIGHT)
+		p->bot = HEIGHT;
+	p->step = (float)p->current_texture->height / wall;
+	p->tex_y = 0;
+	if (p->top == 0)
+		p->tex_y = -(HEIGHT / 2 - wall / 2) * p->step;
+	ft_peint(f, col, p);
+	//hay que liberar la estructura p
 }
