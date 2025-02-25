a63a4f998d32e7fe07af0b2eef577c7e638127fc
Paula <pausanpi1606@gmail.com>
2025-02-25 13:27:33 +0100

    frees

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: pausanch <pausanch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 12:48:18 by pausanch          #+#    #+#             */
-/*   Updated: 2025/02/25 11:36:19 by pausanch         ###   ########.fr       */
+/*   Updated: 2025/02/25 13:07:00 by pausanch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,23 @@
 
 int	check_textures(t_data *data, char **texture)
 {
-	while (data->line && data->line[0] != '1' && data->line[0] != ' ')
+	while (data->line && data->line[0] != '1' && data->line[0] != ' ' && data->line[0] != '\n')
 	{
 		if ((ft_strncmp(data->line, "NO", 2) || ft_strncmp(data->line, "SO", 2)
 				|| ft_strncmp(data->line, "WE", 2)
 				|| ft_strncmp(data->line, "EA", 2)) && (data->line[2] == ' '
 				|| (data->line[2] >= 9 && data->line[3] <= 13)))
+		{
 			gnl_texture(data, texture);
+			data->texture_count++;
+		}
 		else if ((ft_strncmp(data->line, "F", 1)
 				|| ft_strncmp(data->line, "C", 1)) && (data->line[1] == ' '
 				|| (data->line[1] >= 9 && data->line[2] <= 13)))
+		{
 			gnl_texture(data, texture);
+			data->texture_count++;	
+		}
 		free(data->line);
 		data->line = get_next_line(data->fd);
 		if (data->line && data->line[0] >= 9 && data->line[0] <= 13)
@@ -54,8 +60,8 @@ int	parser(t_data *data, char *file)
 		return (printf("Error: Empty file\n"), 1);
 	if (check_textures(data, &texture) == 1)
 		return (free(texture), 1);
-	if (load_textures(data, texture, 0, 0))
-		return (free(texture), 1);
+	if (load_textures(data, texture, 0, 0) == 1)
+		return (1);
 	if (!data->no || !data->so || !data->ea || !data->we)
 		return (free(data->line), print_error("Missing textures"), 1);
 	if (!data->ceiling || !data->floor)
